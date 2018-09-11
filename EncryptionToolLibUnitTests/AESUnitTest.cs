d302d772fe191c2bc6b3b2b83c9528ededc04011
Cody Morterud <CodyMorterud@gmail.com>
2018-09-11 08:57:53 -0400

    Added unit tests.

--- EncryptionToolLibUnitTests/AESUnitTest.cs
+++ EncryptionToolLibUnitTests/AESUnitTest.cs
@@ -7,21 +7,36 @@ namespace EncryptionToolLibUnitTests
     [TestClass]
     public class AESUnitTest
     {
-        private AESFrontEnd aesService;
+        private AESFrontEnd aesService128;
+        private AESFrontEnd aesService256;
         [TestInitialize]
         public void SetUp()
         {
-            aesService = new AESFrontEnd();
+            aesService128 = new AESFrontEnd(AESFrontEnd.KEYSIZE.AES128);
+            aesService256 = new AESFrontEnd(AESFrontEnd.KEYSIZE.AES256);
+
         }
         [TestMethod]
         public void TestSimple128()
         {
             string text = "hello world";
             string key128 = "AAECAwQFBgcICQoLDA0ODw==";
-            aesService.SetKey(key128);
+            aesService128.SetKey(key128);
 
-            var encryptedText = aesService.Encrypt(text);
-            var decryptedText = aesService.Decrypt(encryptedText);
+            var encryptedText = aesService128.Encrypt(text);
+            var decryptedText = aesService128.Decrypt(encryptedText);
+            Assert.AreEqual(text, decryptedText);
+        }
+
+        [TestMethod]
+        public void TestSimple256()
+        {
+            string text = "hello world";
+            string key256 = "INzelqOUVBoeb+A7fvwhXhCtcf6GKWU+oc1xMRYTWaU=";
+            aesService128.SetKey(key256);
+
+            var encryptedText = aesService128.Encrypt(text);
+            var decryptedText = aesService128.Decrypt(encryptedText);
             Assert.AreEqual(text, decryptedText);
         }
     }
